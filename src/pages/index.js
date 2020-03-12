523c0fb960325a0a05b5dd9650ea96f0866f99ce
Katie Rose <katiefelten@gmail.com>
2020-03-12 18:55:55 -0400

    :package: added react-bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ import Header from '../components/header'
 
 export default () => (
 <div>
-<Header title="Header of index page">
+<Header>
 </Header>
     <h1>Hello world!</h1>
     <Link to="/test/">Go to test page</Link>
-    <img src="https://cdn130.picsart.com/263973985003212.png?r480x480" width="680"></img>
+    <img src="https://cdn130.picsart.com/263973985003212.png?r480x480" alt="doggo" width="680"></img>
 </div>
 );
