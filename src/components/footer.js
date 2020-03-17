d05dba1ebe3ba634d70136e83720a3301ef381d8
Katie Rose <katiefelten@gmail.com>
2020-03-17 11:45:16 -0400

    :zap: added header and footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+    <footer>
+        <div class="container">
+<span className="text-muted">This is our footer container.
+</span>
+        </div>
+    </footer>
+);
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Link } from "gatsby";
-import Header from '../components/header'
+import Header from '../components/header';
+import Footer from '../components/footer';
 
 export default () => (
 <div>
-<Header>
-</Header>
-    <h1>Hello world!</h1>
-    <Link to="/test/">Go to test page</Link>
-    <img src="https://cdn130.picsart.com/263973985003212.png?r480x480" alt="doggo" width="680"></img>
+<Header />
+    <div className="contaienr">
+        This is where our content will go.
+    </div>
+    <Footer />
 </div>
 );
