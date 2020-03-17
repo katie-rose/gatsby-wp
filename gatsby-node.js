d731c7e30aaf45d9434307bb1f42ca2202c3914b
Katie Rose <katiefelten@gmail.com>
2020-03-17 16:44:12 -0400

    :zap: updated gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,3 +11,28 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     });
   }
 };
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return graphql(`
+    {
+      allMarkdownRemark {
+        nodes {
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.nodes.forEach(node => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve('./src/layouts/BlogpostLayout.js'),
+        context: {
+          slug: node.fields.slug
+        }
+      });
+    });
+  });
+};
